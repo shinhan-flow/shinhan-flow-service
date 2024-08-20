2718fe18e5efbd6212556c6e43486918bfc34e91
InGyu Moon <ansdlsrb1121@naver.com>
2024-08-21 02:01:47 +0900

    feat: JWTUtil 클래스 구현(token 생성, token 만료 확인)

---
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/jwt/JWTUtil.java
@@ -0,0 +1,66 @@
+package com.ssafy.shinhanflow.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JWTUtil {
+
+	private SecretKey secretKey;
+
+	public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+
+		secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+			Jwts.SIG.HS256.key().build().getAlgorithm());
+	}
+
+	public String getUserId(String token) {
+
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("username", String.class);
+	}
+
+	public String getCategory(String token) {
+
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("category", String.class);
+	}
+
+	public Boolean isExpired(String token) {
+
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration()
+			.before(new Date());
+	}
+
+	public String createJwt(String category, String userId, Long expiredMs) {
+
+		return Jwts.builder()
+			.claim("category", category)
+			.claim("userId", userId)
+			.issuedAt(new Date(System.currentTimeMillis()))
+			.expiration(new Date(System.currentTimeMillis() + expiredMs))
+			.signWith(secretKey)
+			.compact();
+	}
+}
