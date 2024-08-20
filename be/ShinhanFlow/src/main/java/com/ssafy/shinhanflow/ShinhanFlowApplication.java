bb9993c5a7e065677b2177bd000668e541d24527
minjumost <95904729+minjumost@users.noreply.github.com>
2024-08-20 14:55:57 +0900

    chore: 환경변수 설정

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/ShinhanFlowApplication.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/ShinhanFlowApplication.java
@@ -2,12 +2,14 @@ package com.ssafy.shinhanflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application.yml")
 @SpringBootApplication
 public class ShinhanFlowApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShinhanFlowApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ShinhanFlowApplication.class, args);
+	}
 
 }
