7ba4aae1d0a2670d9f8a911ce4228a3a01645718
InGyu Moon <ansdlsrb1121@naver.com>
2024-08-22 02:28:06 +0900

    feat: spring security, webmvc config 설정

---
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/SecurityConfig.java
@@ -0,0 +1,99 @@
+package com.ssafy.shinhanflow.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.ssafy.shinhanflow.jwt.JWTFilter;
+import com.ssafy.shinhanflow.jwt.JWTUtil;
+import com.ssafy.shinhanflow.jwt.LoginFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final AuthenticationConfiguration authenticationConfiguration;
+	private final JWTUtil jwtUtil;
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		// cors 설정
+		http
+			.cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+					CorsConfiguration configuration = new CorsConfiguration();
+
+					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+					configuration.setAllowedMethods(Collections.singletonList("*"));
+					configuration.setAllowCredentials(true);
+					configuration.setAllowedHeaders(Collections.singletonList("*"));
+					configuration.setMaxAge(3600L);
+					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+					return configuration;
+				}
+			})));
+
+		// csrf 비활성화
+		http
+			.csrf((auth) -> auth.disable());
+
+		// form login 비활성화
+		http
+			.formLogin((auth) -> auth.disable());
+
+		// http basic 인증 방식 비활성화
+		http
+			.httpBasic((auth) -> auth.disable());
+
+		// 인증 규칙
+		http
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/login").permitAll()
+				.anyRequest().authenticated());
+
+		// JWT 필터 추가
+		http
+			.addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+		// 로그인 처리 필터 추가
+		http
+			.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+				UsernamePasswordAuthenticationFilter.class);
+
+		// 세션 비활성화
+		http
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return http.build();
+	}
+
+}
