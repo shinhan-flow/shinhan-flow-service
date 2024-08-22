ea7c3a131d12260bc68adc929ca97605f5ce20fd
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-23 08:44:31 +0900

    feat: made fetch input and output to be map

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/finance/FinanceService.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/finance/FinanceService.java
@@ -1,31 +1,48 @@
 package com.ssafy.shinhanflow.finance;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import reactor.core.publisher.Mono;
 
 public class FinanceService {
 	@Value("${financialApi.key}")
 	private String apiKey;
 
 	private final WebClient webClient;
+	private final ObjectMapper objectMapper;
 
-	public FinanceService(WebClient webClient) {
+	public FinanceService(WebClient webClient, ObjectMapper objectMapper) {
 		this.webClient = webClient;
+		this.objectMapper = objectMapper;
 	}
 
-	private String fetch(String url, String body) {
+	private Mono<Map<String, Object>> fetch(String url, Map<String, Object> body) throws JsonProcessingException {
+		body.put("apiKey", apiKey);
 		return webClient.post()
 			.uri(url)
-			.header("apiKey", apiKey)
-			.bodyValue(body)
+			.bodyValue(objectMapper.writeValueAsString(body))
 			.retrieve()
 			.bodyToMono(String.class)
-			.block();
+			.map(response -> {
+				try {
+					return objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {
+					});
+				} catch (Exception e) {
+					throw new RuntimeException("Failed to parse JSON to Map", e);
+				}
+			});
 	}
 
-	public String createMember(String userEmail) {
+	public Mono<Map<String, Object>> createMember(String userEmail) throws JsonProcessingException {
 		String url = "https://finopenapi.ssafy.io/ssafy/api/v1/member/";
-		String body = "{\"email\":\"" + userEmail + "\"}";
+		Map<String, Object> body = Map.of("userId", userEmail);
 		return fetch(url, body);
 	}
 }
