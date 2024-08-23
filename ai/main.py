40f97e742af692a0882cc33d70a7dc8f3efa3b89
ghwns82 <junvery2@naver.com>
2024-08-23 21:16:05 +0900

    chore: add api key with dotenv

--- ai/main.py
+++ ai/main.py
@@ -1,8 +1,11 @@
 import os
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = OpenAI(
-    api_key=""
+    api_key=os.getenv("API_KEY")
 )
 
 chat_completion = client.chat.completions.create(
