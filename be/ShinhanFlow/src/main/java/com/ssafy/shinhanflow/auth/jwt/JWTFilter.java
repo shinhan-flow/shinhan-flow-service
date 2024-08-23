85863ef02c1525dd441368279a1a3a655c2d4fc1
InGyu Moon <ansdlsrb1121@naver.com>
2024-08-24 00:20:25 +0900

    refactor: JWTFilter 토큰 검증 및 생성 로직 리팩토링

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/auth/jwt/JWTFilter.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/auth/jwt/JWTFilter.java
@@ -13,6 +13,7 @@ import com.ssafy.shinhanflow.auth.repository.MemberRepository;
 import com.ssafy.shinhanflow.entity.MemberEntity;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,44 +32,35 @@ public class JWTFilter extends OncePerRequestFilter {
 		FilterChain filterChain) throws ServletException, IOException {
 
 		// 헤더에서 Authorization 에 담긴 토큰을 꺼냄
-		String accessToken = request.getHeader("Authorization");
+		String accessToken = extractToken(request);
 
-		if (accessToken != null && accessToken.startsWith("Bearer ")) {
-			accessToken = accessToken.substring(7);
-		} else {
+		// 토큰이 없을 경우
+		if (accessToken == null) {
 			filterChain.doFilter(request, response);
 			return;
 		}
 
-		// TODO: 토큰 에러 발생시 상태코드 정하기
+		// 토큰 검증
 		try {
-			jwtUtil.isExpired(accessToken);
-			String category = jwtUtil.getCategory(accessToken);
-			if (!"access".equals(category)) {
-				throw new Exception();
-			}
+			validateToken(accessToken);
 		} catch (ExpiredJwtException e) { //토큰 만료
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			response.setContentType("application/json");
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write("{\"error\": \"token has expired.\"}");
+			handleException(response, HttpServletResponse.SC_UNAUTHORIZED, "Token has expired.");
 			return;
-		} catch (Exception e) { // 잘못된 토큰
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			response.setContentType("application/json");
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write("{\"error\": \"invalid token.\"}");
+		} catch (JwtException e) { // 잘못된 토큰
+			handleException(response, HttpServletResponse.SC_UNAUTHORIZED, "Invalid token.");
 			return;
 		}
 
 		// 토큰 검증 완료 후 토큰에서 userId 추출
-		String userId = jwtUtil.getUserId(accessToken);
+		long userId = jwtUtil.getUserId(accessToken);
+
+		log.info("userId: {}", userId);
 
 		// 추출한 userId로 회원 정보 조회
-		Optional<MemberEntity> byId = memberRepository.findById(Long.parseLong(userId));
+		Optional<MemberEntity> memberOptional = memberRepository.findById(userId);
 
 		// 회원 정보가 없을 경우
-		if (byId.isEmpty()) {
+		if (memberOptional.isEmpty()) {
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			response.setContentType("application/json");
 			response.setCharacterEncoding("UTF-8");
@@ -77,14 +69,45 @@ public class JWTFilter extends OncePerRequestFilter {
 		}
 
 		// 회원 정보가 있으면 SecurityContextHolder 에 회원 정보 저장
-		MemberEntity memberEntity = byId.get();
+		MemberEntity memberEntity = memberOptional.get();
+		setAuthentication(memberEntity);
 
+		filterChain.doFilter(request, response);
+
+	}
+
+	private String extractToken(HttpServletRequest request) {
+		String header = request.getHeader("Authorization");
+		if (header != null && header.startsWith("Bearer ")) {
+			return header.substring(7);
+		}
+		return null;
+	}
+
+	private void validateToken(String token) {
+		try {
+			jwtUtil.isExpired(token);
+		} catch (ExpiredJwtException e) {
+			throw new JwtException("Refresh token expired");
+		}
+		String category = jwtUtil.getCategory(token);
+		if (!"access".equals(category)) {
+			throw new JwtException("Invalid token category.");
+		}
+	}
+
+	private void handleException(HttpServletResponse response, int status, String message) throws IOException {
+		log.error("error: {}", message);
+		response.setStatus(status);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("{\"error\": \"" + message + "\"}");
+	}
+
+	private void setAuthentication(MemberEntity memberEntity) {
 		CustomUserDetails customUserDetails = new CustomUserDetails(memberEntity);
 		Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
 			customUserDetails.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(authToken);
-
-		filterChain.doFilter(request, response);
-
 	}
 }
