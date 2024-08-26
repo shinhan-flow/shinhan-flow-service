c8512467f5c8fdd185fd0130d60bbe936164d483
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-27 02:34:13 +0900

    feat: implemented ExchangeRateService methods

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/finance/ExchangeRateService.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/finance/ExchangeRateService.java
@@ -2,6 +2,13 @@ package com.ssafy.shinhanflow.service.finance;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.shinhanflow.config.error.ErrorCode;
+import com.ssafy.shinhanflow.config.error.exception.BadRequestException;
+import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRateRequestDto;
+import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRateResponseDto;
+import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRatesRequestDto;
+import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRatesResponseDto;
+import com.ssafy.shinhanflow.dto.finance.header.RequestHeaderDto;
 import com.ssafy.shinhanflow.repository.MemberRepository;
 import com.ssafy.shinhanflow.util.FinanceApiHeaderGenerator;
 import com.ssafy.shinhanflow.util.FinanceApiService;
@@ -13,7 +20,35 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class ExchangeRateService {
-	private final FinanceApiService financeApiFetcher;
+	private final FinanceApiService financeApiService;
 	private final MemberRepository memberRepository;
 	private final FinanceApiHeaderGenerator financeApiHeaderGenerator;
+
+	public ExchangeRatesResponseDto getExchangeRates() {
+		log.info("getExchangeRates");
+
+		// userKey 필요 없음
+		RequestHeaderDto header = financeApiHeaderGenerator.createHeader("exchangeRate", null, "00100");
+		ExchangeRatesRequestDto dto = ExchangeRatesRequestDto.builder()
+			.header(header)
+			.build();
+
+		return financeApiService.getExchangeRates(dto);
+	}
+
+	public ExchangeRateResponseDto getExchangeRate(String currencyCode) {
+		log.info("getExchangeRate - currencyCode: {}", currencyCode);
+		if (currencyCode == null) {
+			throw new BadRequestException(ErrorCode.NULL_REQUIRED_VALUE);
+		}
+
+		// userKey 필요 없음
+		RequestHeaderDto header = financeApiHeaderGenerator.createHeader("exchangeRate", null, "00100");
+		ExchangeRateRequestDto dto = ExchangeRateRequestDto.builder()
+			.header(header)
+			.currency(currencyCode)
+			.build();
+
+		return financeApiService.getExchangeRate(dto);
+	}
 }
