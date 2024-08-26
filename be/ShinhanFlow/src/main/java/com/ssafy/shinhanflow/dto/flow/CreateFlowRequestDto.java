acd663878aa2d687e42b1866be8c7b9ef8ff9d4b
minjumost <95904729+minjumost@users.noreply.github.com>
2024-08-27 02:45:29 +0900

    refactor: 역직렬화/직렬화 방식 변경
    1. 직렬화의 경우 toString메서드처럼 사용했으나, objectMapper를 이용하여 직접 json으로 직렬화하였고
    2. 역직렬화의 경우 jsonproperty를 각 데이터 클래스로 옮겨서 역직렬화 책임을 넘겼습니다.
    3. 디비의 구조도 변경하여 일관된 방식으로 역직렬화를 처리할 수 있도록 변경했습니다.

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/flow/CreateFlowRequestDto.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/flow/CreateFlowRequestDto.java
@@ -1,72 +1,27 @@
 package com.ssafy.shinhanflow.dto.flow;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.util.List;
+
 import com.ssafy.shinhanflow.domain.action.Action;
-import com.ssafy.shinhanflow.domain.action.BalanceNotificationAction;
-import com.ssafy.shinhanflow.domain.action.ExchangeAction;
-import com.ssafy.shinhanflow.domain.action.ExchangeRateNotificationAction;
-import com.ssafy.shinhanflow.domain.action.TextNotificationAction;
-import com.ssafy.shinhanflow.domain.action.TransferAction;
 import com.ssafy.shinhanflow.domain.trigger.Trigger;
-import com.ssafy.shinhanflow.domain.trigger.account.BalanceTrigger;
-import com.ssafy.shinhanflow.domain.trigger.account.DepositTrigger;
-import com.ssafy.shinhanflow.domain.trigger.account.TransferTrigger;
-import com.ssafy.shinhanflow.domain.trigger.account.WithDrawTrigger;
-import com.ssafy.shinhanflow.domain.trigger.date.DayOfMonthTrigger;
-import com.ssafy.shinhanflow.domain.trigger.date.DayOfWeekTrigger;
-import com.ssafy.shinhanflow.domain.trigger.date.MultiDateTrigger;
-import com.ssafy.shinhanflow.domain.trigger.date.PeriodDateTrigger;
-import com.ssafy.shinhanflow.domain.trigger.date.SpecificDateTrigger;
-import com.ssafy.shinhanflow.domain.trigger.exchange.ExchangeRateTrigger;
-import com.ssafy.shinhanflow.domain.trigger.product.InterestRateTrigger;
-import com.ssafy.shinhanflow.domain.trigger.time.SpecificTimeTrigger;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 public record CreateFlowRequestDto(
+	@NotBlank
 	String title,
+	@NotBlank
 	String description,
-	@JsonTypeInfo(
-		use = JsonTypeInfo.Id.NAME,
-		include = JsonTypeInfo.As.PROPERTY,
-		property = "type"
-	)
-	@JsonSubTypes({
-		@JsonSubTypes.Type(value = BalanceTrigger.class, name = "balance"),
-		@JsonSubTypes.Type(value = DepositTrigger.class, name = "deposit"),
-		@JsonSubTypes.Type(value = TransferTrigger.class, name = "transfer"),
-		@JsonSubTypes.Type(value = WithDrawTrigger.class, name = "withDraw"),
-		@JsonSubTypes.Type(value = DayOfWeekTrigger.class, name = "dayOfWeek"),
-		@JsonSubTypes.Type(value = DayOfMonthTrigger.class, name = "dayOfMonth"),
-		@JsonSubTypes.Type(value = MultiDateTrigger.class, name = "multiDate"),
-		@JsonSubTypes.Type(value = PeriodDateTrigger.class, name = "periodDate"),
-		@JsonSubTypes.Type(value = SpecificDateTrigger.class, name = "specificDate"),
-		@JsonSubTypes.Type(value = ExchangeRateTrigger.class, name = "exchangeRate"),
-		@JsonSubTypes.Type(value = InterestRateTrigger.class, name = "interestRate"),
-		@JsonSubTypes.Type(value = SpecificTimeTrigger.class, name = "specificTime")
-	})
-	@NotEmpty
-	@Valid
-	Trigger[] triggers,
 
-	@JsonTypeInfo(
-		use = JsonTypeInfo.Id.NAME,
-		include = JsonTypeInfo.As.PROPERTY,
-		property = "type",
-		visible = true
-	)
-	@JsonSubTypes({
-		@JsonSubTypes.Type(value = TextNotificationAction.class, name = "textNotification"),
-		@JsonSubTypes.Type(value = BalanceNotificationAction.class, name = "balanceNotification"),
-		@JsonSubTypes.Type(value = ExchangeRateNotificationAction.class, name = "exchangeRateNotification"),
-		@JsonSubTypes.Type(value = TransferAction.class, name = "transfer"),
-		@JsonSubTypes.Type(value = ExchangeAction.class, name = "exchange")
-	})
 	@NotEmpty
 	@Valid
-	Action[] actions
+	List<Trigger> triggers,
+
+	@NotEmpty
+	@Valid
+	List<Action> actions
 ) {
 
 }
--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/flow/FlowService.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/flow/FlowService.java
@@ -1,12 +1,13 @@
 package com.ssafy.shinhanflow.service.flow;
 
-import java.util.Optional;
-import java.util.concurrent.Flow;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.ssafy.shinhanflow.config.error.ErrorCode;
-import com.ssafy.shinhanflow.config.error.exception.BadRequestException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ssafy.shinhanflow.domain.action.Action;
 import com.ssafy.shinhanflow.domain.entity.ActionEntity;
 import com.ssafy.shinhanflow.domain.entity.FlowEntity;
@@ -26,47 +27,69 @@ public class FlowService {
 	private final FlowRepository flowRepository;
 	private final TriggerRepository triggerRepository;
 	private final ActionRepository actionRepository;
+	private final ObjectMapper objectMapper;
 
 	@Transactional
 	public Boolean createFlow(Long memberId, CreateFlowRequestDto createFlowRequestDto) {
 
-		// flow 생성
-		FlowEntity flowEntity = FlowEntity
-			.builder()
-			.memberId(memberId)
-			.title(createFlowRequestDto.title())
-			.description(createFlowRequestDto.description())
-			.build();
+		try {
 
-		FlowEntity flow =  flowRepository.save(flowEntity);
-
-		// trigger 생성
-		Trigger[] triggers = createFlowRequestDto.triggers();
-		for(Trigger trigger : triggers) {
-			TriggerEntity triggerEntity = TriggerEntity
-				.builder()
-				.flowId(flow.getId())
-				.memberId(memberId)
-				.type(trigger.getClass().getSimpleName())
-				.data(trigger.toJson())
-				.build();
-
-			triggerRepository.save(triggerEntity);
-		}
-
-		// action 생성
-		Action[] actions = createFlowRequestDto.actions();
-		for(Action action : actions) {
-			ActionEntity actionEntity = ActionEntity
+			// flow 생성
+			FlowEntity flowEntity = FlowEntity
 				.builder()
 				.memberId(memberId)
-				.flowId(flow.getId())
-				.type(action.getClass().getSimpleName())
-				.data(action.toJson())
+				.title(createFlowRequestDto.title())
+				.description(createFlowRequestDto.description())
 				.build();
 
-			actionRepository.save(actionEntity);
+			FlowEntity flow = flowRepository.save(flowEntity);
+
+			// trigger 생성
+			List<Trigger> triggers = createFlowRequestDto.triggers();
+			for (Trigger trigger : triggers) {
+
+				String jsonString = objectMapper.writeValueAsString(trigger);
+				JsonNode jsonNode = objectMapper.readTree(jsonString);
+				ObjectNode objectNode = (ObjectNode)jsonNode;
+				objectNode.put("type", trigger.getClass().getSimpleName());
+				jsonString = objectMapper.writeValueAsString(objectNode);
+
+				TriggerEntity triggerEntity = TriggerEntity
+					.builder()
+					.flowId(flow.getId())
+					.memberId(memberId)
+					.type(trigger.getClass().getSimpleName())
+					.data(jsonString)
+					.build();
+
+				triggerRepository.save(triggerEntity);
+
+			}
+
+			// action 셍성
+			List<Action> actions = createFlowRequestDto.actions();
+			for (Action action : actions) {
+				String jsonString = objectMapper.writeValueAsString(action);
+				JsonNode jsonNode = objectMapper.readTree(jsonString);
+				ObjectNode objectNode = (ObjectNode)jsonNode;
+				objectNode.put("type", action.getClass().getSimpleName());
+				jsonString = objectMapper.writeValueAsString(objectNode);
+
+				ActionEntity actionEntity = ActionEntity
+					.builder()
+					.memberId(memberId)
+					.flowId(flow.getId())
+
+					.type(action.getClass().getSimpleName())
+					.data(jsonString)
+					.build();
+
+				actionRepository.save(actionEntity);
+			}
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
 		}
+
 		return true;
 	}
 }
