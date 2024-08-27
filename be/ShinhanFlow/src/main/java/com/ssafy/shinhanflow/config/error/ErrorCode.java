63fd99be0c8f2f1c0d8a201c77ee3b017600e000
최민주 <minjumost@naver.com>
2024-08-27 10:39:32 +0900

    feat: 예외처리 추가

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/error/ErrorCode.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/error/ErrorCode.java
@@ -14,15 +14,16 @@ import lombok.Getter;
  * */
 @Getter
 public enum ErrorCode {
+	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "4000", "요청하신 리소스를 찾을 수 없습니다."),
 	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "4001", "잘못된 HTTP 메서드를 호출했습니다."),
 	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "4002", "올바르지 않은 입력값입니다."),
 	NOT_FOUND(HttpStatus.NOT_FOUND, "4003", "존재하지 않는 엔티티입니다."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5001", "서버 에러가 발생했습니다."),
 	INVALID_TOKEN(HttpStatus.BAD_REQUEST, "4004", "토큰이 유효하지 않습니다."),
 	EXPIRED_TOKEN(HttpStatus.BAD_REQUEST, "4005", "토큰이 만료됐습니다."),
 	NULL_REQUIRED_VALUE(HttpStatus.BAD_REQUEST, "4006", "필수 요청값이 비어있습니다."),
-	;
 
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5001", "서버 에러가 발생했습니다."),
+	;
 	private final HttpStatus status;
 	private final String message;
 	private final String code;
--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/error/exception/GlobalExceptionHandler.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/error/exception/GlobalExceptionHandler.java
@@ -1,30 +1,70 @@
 package com.ssafy.shinhanflow.config.error.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.ssafy.shinhanflow.config.error.ErrorCode;
 import com.ssafy.shinhanflow.config.error.ErrorResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
+
+/**
+ * @ControllerAdvice를 통해 모든 컨트롤러에서 발생하는 모든 예외를 처리할 수 있음.
+ * 컨트롤러에서 호출하는 서비스로직도 포함.
+ */
 @Slf4j
-@ControllerAdvice // 모든 컨트롤러에서 발생하는 예외를 잡아서 처리
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+	/**
+	 * 라우팅 되지않은 자원에 접근하거나, 잘못된 method를 호출하는 예외
+	 */
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	protected ResponseEntity<ErrorResponse> handle(HttpRequestMethodNotSupportedException e) {
 		log.error("HttpRequestMethodNotSupportedException", e);
 		return createErrorResponseEntity(ErrorCode.METHOD_NOT_ALLOWED);
 	}
 
+	@ExceptionHandler(NoResourceFoundException.class)
+	protected ResponseEntity<ErrorResponse> handle(NoResourceFoundException e){
+		log.error("NoResourceFoundException", e);
+		return createErrorResponseEntity(ErrorCode.RESOURCE_NOT_FOUND);
+	}
+
+	/**
+	 * 유효성을 검증하는 @Valid에서 발생시키는 예외
+	 */
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	protected ResponseEntity<ErrorResponse> handle(HttpMessageNotReadableException e) {
+		log.error("HttpMessageNotReadableException", e);
+		return createErrorResponseEntity(ErrorCode.INVALID_INPUT_VALUE);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+		log.error("HttpMessageNotReadableException", e);
+		return createErrorResponseEntity(ErrorCode.INVALID_INPUT_VALUE);
+	}
+
+	/**
+	 * 서비스 로직에서 직접 throw하는 커스텀 예외
+	 */
 	@ExceptionHandler(BusinessBaseException.class)
 	protected ResponseEntity<ErrorResponse> handle(BusinessBaseException e) {
 		log.error("BusinessException", e);
 		return createErrorResponseEntity(e.getErrorCode());
 	}
 
+	/**
+	 *	금융 API 에서 응답받은 예외
+	 */
 	@ExceptionHandler(FinanceApiException.class)
 	protected ResponseEntity<ErrorResponse> handle(FinanceApiException e) {
 		log.error("FinanceApiException", e);
@@ -36,13 +76,16 @@ public class GlobalExceptionHandler {
 		);
 	}
 
+	/**
+	 * 기타 정의되지 않은 예외는 서버 에러로 처리
+	 */
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ErrorResponse> handle(Exception e) {
-		e.printStackTrace();
 		log.error("Exception", e);
 		return createErrorResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR);
 	}
 
+	// 공용 메서드
 	private ResponseEntity<ErrorResponse> createErrorResponseEntity(ErrorCode errorCode) {
 		return new ResponseEntity<>(
 			ErrorResponse.of(errorCode),
