31f549ca9162ef8a2ba7ec5ea463916bc384a878
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-27 10:57:28 +0900

    fix: remove not used lines JacksonConfig.java

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/JacksonConfig.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/config/JacksonConfig.java
@@ -9,22 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.ssafy.shinhanflow.util.CustomBigDecimalDeserializer;
 
-// @Configuration
-// public class JacksonConfig {
-//
-// 	@Bean
-// 	public Jackson2ObjectMapperBuilderCustomizer customizer() {
-// 		return new Jackson2ObjectMapperBuilderCustomizer() {
-// 			@Override
-// 			public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
-// 				SimpleModule module = new SimpleModule();
-// 				module.addDeserializer(BigDecimal.class, new CustomBigDecimalComponent.Deserializer());
-// 				jacksonObjectMapperBuilder.modules(module);
-// 			}
-// 		};
-// 	}
-// }
-
 @Configuration
 public class JacksonConfig {
 
