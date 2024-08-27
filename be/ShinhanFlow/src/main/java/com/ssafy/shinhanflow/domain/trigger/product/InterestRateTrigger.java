fb00acd90f4c1619c8333d4966a55dc95e445ac3
InGyu Moon <ansdlsrb1121@naver.com>
2024-08-27 11:07:04 +0900

    feat: InterestRateTrigger 클래스 isTriggered 구현

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/domain/trigger/product/InterestRateTrigger.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/domain/trigger/product/InterestRateTrigger.java
@@ -1,16 +1,20 @@
 package com.ssafy.shinhanflow.domain.trigger.product;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.ssafy.shinhanflow.config.error.ErrorCode;
+import com.ssafy.shinhanflow.config.error.exception.BadRequestException;
 import com.ssafy.shinhanflow.domain.trigger.Trigger;
+import com.ssafy.shinhanflow.service.finance.FinanceProductService;
 import com.ssafy.shinhanflow.util.constants.AccountProduct;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record InterestRateTrigger(
+	FinanceProductService financeProductService,
 	@NotNull
 	AccountProduct accountProduct,
 	@NotNull
@@ -21,13 +25,31 @@ public record InterestRateTrigger(
 
 	@Override
 	public boolean isTriggered() {
-		// todo: product에 맞는 상품 조회
-		List<BigDecimal> rates = new ArrayList<>();
-		for (BigDecimal rate : rates) {
-			if (this.rate.compareTo(rate) >= 0) {
-				return true;
-			}
-		}
-		return false;
+
+		List<BigDecimal> rates = getProductRates(accountProduct);
+
+		return rates.stream().anyMatch(rate -> this.rate.compareTo(rate) >= 0);
+	}
+
+	private List<BigDecimal> getProductRates(AccountProduct accountProduct) {
+		return switch (accountProduct) {
+			case DEPOSIT_ACCOUNT -> financeProductService.depositProductsInfo()
+				.getRec()
+				.stream()
+				.map(rec -> BigDecimal.valueOf(rec.interestRate()))
+				.collect(Collectors.toList());
+			case SAVING_ACCOUNT -> financeProductService.savingProductsInfo()
+				.getRec()
+				.stream()
+				.map(rec -> BigDecimal.valueOf(rec.interestRate()))
+				.collect(Collectors.toList());
+			case LOAN_ACCOUNT -> financeProductService.loanProductsInfo()
+				.getRec()
+				.stream()
+				.map(rec -> BigDecimal.valueOf(rec.interestRate()).negate()) // 대출 금리는 음수로 변환
+				.collect(Collectors.toList());
+			// CURRENT_ACCOUNT type 은 interestRate 이 없음
+			default -> throw new BadRequestException(ErrorCode.INVALID_ACCOUNT_TYPE);
+		};
 	}
 }
