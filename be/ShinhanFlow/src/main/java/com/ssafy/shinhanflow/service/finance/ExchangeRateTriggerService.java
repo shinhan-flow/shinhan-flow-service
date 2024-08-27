c2b9ad6e72a81a4c67e24fd7f0869603ca6e981a
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-27 23:13:16 +0900

    chore: remove unused fields and methods

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/finance/ExchangeRateTriggerService.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/finance/ExchangeRateTriggerService.java
@@ -6,10 +6,7 @@ import com.ssafy.shinhanflow.config.error.ErrorCode;
 import com.ssafy.shinhanflow.config.error.exception.BadRequestException;
 import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRateRequestDto;
 import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRateResponseDto;
-import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRatesRequestDto;
-import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRatesResponseDto;
 import com.ssafy.shinhanflow.dto.finance.header.RequestHeaderDto;
-import com.ssafy.shinhanflow.repository.MemberRepository;
 import com.ssafy.shinhanflow.service.TriggerService;
 import com.ssafy.shinhanflow.util.FinanceApiHeaderGenerator;
 import com.ssafy.shinhanflow.util.FinanceApiService;
@@ -23,20 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ExchangeRateTriggerService extends TriggerService {
 	private final FinanceApiService financeApiService;
-	private final MemberRepository memberRepository;
 	private final FinanceApiHeaderGenerator financeApiHeaderGenerator;
-
-	public ExchangeRatesResponseDto getExchangeRates() {
-		log.info("getExchangeRates");
-
-		// userKey 필요 없음
-		RequestHeaderDto header = financeApiHeaderGenerator.createHeader("exchangeRate", null, "00100");
-		ExchangeRatesRequestDto dto = ExchangeRatesRequestDto.builder()
-			.header(header)
-			.build();
-
-		return financeApiService.getExchangeRates(dto);
-	}
 
 	public ExchangeRateResponseDto getExchangeRate(String currencyCode) {
 		log.info("getExchangeRate - currencyCode: {}", currencyCode);
