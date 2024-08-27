5a0414c4cfa8c411c084e6028c9c72518291d394
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-27 23:11:25 +0900

    feat: implemented ExchangeRateTrigger

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/domain/trigger/exchange/ExchangeRateTrigger.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/domain/trigger/exchange/ExchangeRateTrigger.java
@@ -3,7 +3,9 @@ package com.ssafy.shinhanflow.domain.trigger.exchange;
 import java.math.BigDecimal;
 
 import com.ssafy.shinhanflow.domain.trigger.Trigger;
-import com.ssafy.shinhanflow.service.finance.FinanceTriggerService;
+import com.ssafy.shinhanflow.dto.finance.exchange.ExchangeRateResponseDto;
+import com.ssafy.shinhanflow.service.TriggerService;
+import com.ssafy.shinhanflow.service.finance.ExchangeRateTriggerService;
 import com.ssafy.shinhanflow.util.constants.Currency;
 
 import jakarta.validation.constraints.NotNull;
@@ -19,10 +21,14 @@ public record ExchangeRateTrigger(
 ) implements Trigger {
 
 	@Override
-	public boolean isTriggered(FinanceTriggerService financeTriggerService) {
-		// todo: currency로 환율 가져오기
-		BigDecimal rate = BigDecimal.valueOf(2.02);
-		return this.rate.compareTo(rate) >= 0;
+	public boolean isTriggered(TriggerService triggerService) {
+		if (triggerService instanceof ExchangeRateTriggerService exchangeRateTriggerService) {
+			ExchangeRateResponseDto dto = exchangeRateTriggerService.getExchangeRate(currency.name());
+			BigDecimal rate = dto.getRec().getExchangeRate();
+			return this.rate.compareTo(rate) >= 0;
+		}
+
+		throw new IllegalArgumentException("Invalid TriggerService");
 	}
 
 }
