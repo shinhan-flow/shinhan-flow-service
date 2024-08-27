d2f1b12c9072cee723e0bde50f520c652c7ca276
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-28 01:17:12 +0900

    feat: implemented dtos

---
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/finance/exchange/ExchangeRequestDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.shinhanflow.dto.finance.exchange;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.shinhanflow.dto.finance.FinanceApiRequestDto;
+import com.ssafy.shinhanflow.dto.finance.header.RequestHeaderDto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+@EqualsAndHashCode(callSuper = true)
+@Value
+public class ExchangeRequestDto extends FinanceApiRequestDto {
+	@JsonProperty("Header")
+	RequestHeaderDto header;
+	String accountNo;
+	String exchangeCurrency;
+	/**
+	 * Double 이지만 단위는 10이다.
+	 */
+	Double exchangeAmount;
+}
