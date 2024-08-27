3ab70b49702f5d0e6fc2d2b4771859c4b97e2440
gwonhong <gwonhong.dev@gmail.com>
2024-08-27 11:18:32 +0900

    feat: use Currency enum for currency field

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/finance/exchange/ExchangeRateRequestDto.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/finance/exchange/ExchangeRateRequestDto.java
@@ -3,6 +3,7 @@ package com.ssafy.shinhanflow.dto.finance.exchange;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.shinhanflow.dto.finance.FinanceApiRequestDto;
 import com.ssafy.shinhanflow.dto.finance.header.RequestHeaderDto;
+import com.ssafy.shinhanflow.util.constants.Currency;
 
 import lombok.Builder;
 import lombok.Value;
@@ -12,5 +13,5 @@ import lombok.Value;
 public class ExchangeRateRequestDto extends FinanceApiRequestDto {
 	@JsonProperty("Header")
 	RequestHeaderDto header;
-	String currency;
+	Currency currency;
 }
