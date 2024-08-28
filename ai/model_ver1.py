0656bf89c0d704ddd893c553a9c49bcbf12f6850
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2024-08-28 09:45:35 +0900

    feat : 프롬프트 프로토타입 개발

--- ai/model_ver1.py
+++ ai/model_ver1.py
@@ -5,13 +5,24 @@ import json
 import sys
 
 mypath = sys.argv[0]
-MY_REQUEST = sys.argv[1]
+# MY_REQUEST = sys.argv[1]
+MY_REQUEST = "매월 25일에 5만원을 111-113-111111 계좌에서 123-456-123123 계좌로 송금" 
+
 root_path, myfile = os.path.split(mypath)
 
 
 
-with open(f'{root_path}/prompt/system_ver1.json','r') as f:
-    data = json.load(f)
+with open(f'{root_path}/prompt/explain_flow.json','r') as f:
+    explain_flow = json.load(f)
+with open(f'{root_path}/prompt/enumerate_data_options.json','r') as f:
+    enumerate_data_options = json.load(f)
+with open(f'{root_path}/prompt/enumerate_triggers.json','r') as f:
+    enumerate_triggers = json.load(f)
+with open(f'{root_path}/prompt/enumerate_actions.json','r') as f:
+    enumerate_actions = json.load(f)
+with open(f'{root_path}/prompt/set_format.json','r') as f:
+    set_format= json.load(f)
+system_prompt = [explain_flow, enumerate_data_options, enumerate_triggers, enumerate_actions, set_format]
 
 load_dotenv()
 
@@ -21,7 +32,7 @@ client = OpenAI(
 
 chat_completion = client.chat.completions.create(
     messages=[
-        data,
+        *system_prompt,
         {
             "role": "user",
             "content": MY_REQUEST,
