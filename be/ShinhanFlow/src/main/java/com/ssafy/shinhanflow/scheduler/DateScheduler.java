fffc3721fc1f3399d91118f151c50a39263a8642
minjumost <95904729+minjumost@users.noreply.github.com>
2024-08-29 02:05:22 +0900

    feat: 플로우 스케줄링
    스케줄링을 통해 트리거를 확인한 후 해당 플로우의 트리거가 모두 참을 반환하면 연결된 액션을 모두 실행합니다.
    이때 flow가 enable상태여야하며,
    트리거는 하루에 한번씩만 실행되기 때문에 날짜 제한을 뒀습니다.

---
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/scheduler/DateScheduler.java
@@ -0,0 +1,23 @@
+package com.ssafy.shinhanflow.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.shinhanflow.repository.TriggerRepository;
+import com.ssafy.shinhanflow.util.TriggerChecker;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DateScheduler {
+
+	private final TriggerChecker triggerChecker;
+	private final TriggerRepository triggerRepository;
+
+	@Scheduled(cron = "*/5 * * * * *")
+	public void checkDateTrigger() {
+		triggerChecker.run();
+	}
+
+}
