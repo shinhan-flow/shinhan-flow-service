5210a758c296fffcfa528c5580a9170ba9150efd
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-30 00:14:27 +0900

    feat: implemented exchange action

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/domain/action/ExchangeAction.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/domain/action/ExchangeAction.java
@@ -1,12 +1,15 @@
 package com.ssafy.shinhanflow.domain.action;
 
+import com.ssafy.shinhanflow.service.flow.FinanceActionService;
 import com.ssafy.shinhanflow.util.constants.Currency;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Builder
 public record ExchangeAction(
 	@NotNull
@@ -21,7 +24,8 @@ public record ExchangeAction(
 
 ) implements Action {
 	@Override
-	public boolean execute() {
-		return false;
+	public boolean execute(FinanceActionService financeActionService, Long userId) {
+		financeActionService.exchange(userId, fromAccount, currency.name(), amount.intValue());
+		return true;
 	}
 }
