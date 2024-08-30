16ee42b45825a46f998587125b71daaf24b5ba97
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-31 01:08:46 +0900

    feat: add Builder annotation to dto

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/ai/FlowGeneratorResponseDto.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/dto/ai/FlowGeneratorResponseDto.java
@@ -2,5 +2,8 @@ package com.ssafy.shinhanflow.dto.ai;
 
 import com.ssafy.shinhanflow.dto.flow.CreateFlowRequestDto;
 
+import lombok.Builder;
+
+@Builder
 public record FlowGeneratorResponseDto(CreateFlowRequestDto flow) {
 }
