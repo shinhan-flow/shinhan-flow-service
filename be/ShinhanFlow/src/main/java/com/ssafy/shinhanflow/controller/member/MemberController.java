973e2ee5641365da5326c7343666c4987395fcc0
InGyu Moon <ansdlsrb1121@naver.com>
2024-08-30 20:11:58 +0900

    feat: 신용점수 확인 api 구현

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/controller/member/MemberController.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/controller/member/MemberController.java
@@ -1,11 +1,14 @@
 package com.ssafy.shinhanflow.controller.member;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.shinhanflow.config.error.SuccessResponse;
+import com.ssafy.shinhanflow.dto.member.CreditScoreResponseDto;
 import com.ssafy.shinhanflow.dto.member.SignUpRequestDto;
 import com.ssafy.shinhanflow.service.member.MemberService;
 
@@ -21,4 +24,11 @@ public class MemberController {
 	public SuccessResponse<Boolean> createMember(@RequestBody SignUpRequestDto signUpRequestDto) {
 		return SuccessResponse.of(memberService.createMember(signUpRequestDto));
 	}
+
+	@GetMapping("/credit-score")
+	public SuccessResponse<CreditScoreResponseDto> getCreditScore(@RequestHeader("Authorization") String token) {
+		return SuccessResponse.of(memberService.getCreditScore(token));
+	}
 }
+
+
--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/member/MemberService.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/member/MemberService.java
@@ -1,14 +1,23 @@
 package com.ssafy.shinhanflow.service.member;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.shinhanflow.auth.jwt.JWTUtil;
 import com.ssafy.shinhanflow.config.SecurityConfig;
+import com.ssafy.shinhanflow.config.error.ErrorCode;
+import com.ssafy.shinhanflow.config.error.exception.BadRequestException;
+import com.ssafy.shinhanflow.domain.entity.MemberEntity;
 import com.ssafy.shinhanflow.dto.finance.MemberRequestDto;
 import com.ssafy.shinhanflow.dto.finance.MemberResponseDto;
+import com.ssafy.shinhanflow.dto.finance.header.RequestHeaderDto;
+import com.ssafy.shinhanflow.dto.member.CreditScoreRequestDto;
+import com.ssafy.shinhanflow.dto.member.CreditScoreResponseDto;
 import com.ssafy.shinhanflow.dto.member.SignUpRequestDto;
-import com.ssafy.shinhanflow.domain.entity.MemberEntity;
 import com.ssafy.shinhanflow.repository.MemberRepository;
+import com.ssafy.shinhanflow.util.FinanceApiHeaderGenerator;
 import com.ssafy.shinhanflow.util.FinanceApiService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +30,8 @@ public class MemberService {
 	private final MemberRepository memberRepository;
 	private final FinanceApiService financeApiFetcher;
 	private final SecurityConfig securityConfig;
+	private final FinanceApiHeaderGenerator financeApiHeaderGenerator;
+	private final JWTUtil jwtUtil;
 
 	public Boolean createMember(SignUpRequestDto signUpRequestDto) {
 		// communicate with finance api
@@ -36,5 +47,28 @@ public class MemberService {
 		memberRepository.save(memberEntity);
 		// return response
 		return true;
+	}
+
+	public CreditScoreResponseDto getCreditScore(String token) {
+		long userId = jwtUtil.getId(token);
+		MemberEntity memberEntity = findMemberOrThrow(userId);
+
+		String userKey = memberEntity.getUserKey();
+		String institutionCode = memberEntity.getInstitutionCode();
+		RequestHeaderDto header = financeApiHeaderGenerator.createHeader("inquireMyCreditRating", userKey,
+			institutionCode);
+		CreditScoreRequestDto dto = CreditScoreRequestDto.builder()
+			.header(header)
+			.build();
+		return financeApiFetcher.getCreditScore(dto);
+	}
+
+	private MemberEntity findMemberOrThrow(long userId) {
+		Optional<MemberEntity> memberEntityOptional = memberRepository.findById(userId);
+		if (memberEntityOptional.isEmpty()) {
+			throw new BadRequestException(ErrorCode.NOT_FOUND);
+		} else {
+			return memberEntityOptional.get();
+		}
 	}
 }
