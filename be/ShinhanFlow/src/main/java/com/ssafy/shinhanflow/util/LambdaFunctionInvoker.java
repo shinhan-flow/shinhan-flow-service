f59c98191e482292e924f422b8a476574f054f32
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-31 03:09:26 +0900

    fix: made invokeFunction to properly return response body

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/util/LambdaFunctionInvoker.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/util/LambdaFunctionInvoker.java
@@ -2,6 +2,8 @@ package com.ssafy.shinhanflow.util;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.core.SdkBytes;
@@ -14,6 +16,7 @@ import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 @RequiredArgsConstructor
 public class LambdaFunctionInvoker {
 	private final LambdaClient lambdaClient;
+	private final ObjectMapper objectMapper;
 
 	public String invokeFunction(String functionName, String payload) {
 		log.info("LambdaFunctionInvoker.invokeFunction - functionName: {}, payload: {}", functionName, payload);
@@ -23,6 +26,15 @@ public class LambdaFunctionInvoker {
 			.build();
 
 		InvokeResponse invokeResponse = lambdaClient.invoke(invokeRequest);
-		return invokeResponse.payload().asUtf8String();
+		return getInvokeResponseBody(invokeResponse.payload().asUtf8String());
+	}
+
+	private String getInvokeResponseBody(String invokeResponseString) {
+		try {
+			return objectMapper.readTree(invokeResponseString).get("body").asText();
+		} catch (Exception e) {
+			throw new RuntimeException("LambdaFunctionInvoker.getInvokeResponseBody - Failed to parse response body",
+				e);
+		}
 	}
 }
