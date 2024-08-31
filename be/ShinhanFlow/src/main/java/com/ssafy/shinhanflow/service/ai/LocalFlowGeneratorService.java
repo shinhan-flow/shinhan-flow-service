2318a35392f5b4a789913da62440e7aca8f73dfa
Gwon Hong <gwonhong.dev@gmail.com>
2024-08-31 16:47:03 +0900

    fix: made unused api silent

--- be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/ai/LocalFlowGeneratorService.java
+++ be/ShinhanFlow/src/main/java/com/ssafy/shinhanflow/service/ai/LocalFlowGeneratorService.java
@@ -1,7 +1,5 @@
 package com.ssafy.shinhanflow.service.ai;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,19 +13,19 @@ import lombok.RequiredArgsConstructor;
 public class LocalFlowGeneratorService {
 	// @Value("${openai.api-key}")
 	// private String apiKey;
-	@Value("${server-url}")
-	private String serverUrl;
+	private String serverUrl = "";
 
 	private final WebClient webClient;
 	private final ObjectMapper objectMapper;
 
 	public CreateFlowRequestDto generateFlow(Long userId, String prompt) {
-		return webClient.post()
-			.uri(serverUrl)
-			.contentType(MediaType.APPLICATION_JSON)
-			.bodyValue("{\"input_string\": \"%s\"}".formatted(prompt))
-			.retrieve()
-			.bodyToMono(CreateFlowRequestDto.class)
-			.block();
+		// return webClient.post()
+		// 	.uri(serverUrl)
+		// 	.contentType(MediaType.APPLICATION_JSON)
+		// 	.bodyValue("{\"input_string\": \"%s\"}".formatted(prompt))
+		// 	.retrieve()
+		// 	.bodyToMono(CreateFlowRequestDto.class)
+		// 	.block();
+		return null;
 	}
 }
